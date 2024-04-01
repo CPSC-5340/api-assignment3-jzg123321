4df81c061c9fe38ef76382a152c4bbe0b4e93860
jzg123321 <123005578+jzg123321@users.noreply.github.com>
2024-03-31 23:47:16 -0500

    Add files via upload

---
+++ Assignment3App.swift
@@ -0,0 +1,19 @@
+//
+//  ContentView.swift
+//  Assignment3app
+//
+//  Created by Josh Guiang on 3/29/24.
+//
+
+import SwiftUI
+
+@main
+struct Assignment3App: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                MainInputView()
+            }
+        }
+    }
+}
